3e3bc5efb8cb0d946e2e02272e1f1bf700158102
Richard Clamp <richardc@unixbeard.net>
2013-09-30 19:30:49 +0100

    load the json encoder

--- mgo/load.go
+++ mgo/load.go
@@ -4,3 +4,4 @@ package main
 import _ "github.com/richardc/mgollective/mgo_connector_redis"
 import _ "github.com/richardc/mgollective/mgo_connector_activemq"
 import _ "github.com/richardc/mgollective/mgo_securityprovider_psk"
+import _ "github.com/richardc/mgollective/mgo_encoder_json"

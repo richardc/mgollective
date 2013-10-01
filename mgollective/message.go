dff8b8f193130e47ba0d7a2b4f12422d728975d3
Richard Clamp <richardc@unixbeard.net>
2013-10-01 11:02:09 +0100

    specify json field names

--- mgollective/message.go
+++ mgollective/message.go
@@ -20,33 +20,33 @@ type WireMessage struct {
 }
 
 type Filter struct {
-	Key     string
-	Operand string
-	Value   string
+	Key     string `json:"key"`
+	Operand string `json:"operand"`
+	Value   string `json:"value"`
 }
 
 type RequestFilters struct {
-	Identity []Filter
-	Agent    []Filter
-	Compound []Filter
-	Cf_class []Filter
+	Identity []Filter `json:"identity"`
+	Agent    []Filter `json:"agent"`
+	Compound []Filter `json:"compound"`
+	Cf_class []Filter `json:"cf_class"`
 }
 
 type RequestBody struct {
-	Agent  string
-	Action string
-	Params map[string]string
+	Agent  string            `json:"agent"`
+	Action string            `json:"action"`
+	Params map[string]string `json:"params"`
 }
 
 type RequestMessage struct {
-	Headers map[string]string
-	Filters RequestFilters
-	Body    RequestBody
+	Headers map[string]string `json:"headers"`
+	Filters RequestFilters    `json:"filters"`
+	Body    RequestBody       `json:"body"`
 }
 
 type ResponseBody map[string]string
 
 type ResponseMessage struct {
-	Headers map[string]string
-	Body    ResponseBody
+	Headers map[string]string `json:"headers"`
+	Body    ResponseBody      `json:"body"`
 }

0d535d85c00d9d19f44a87b394495dc5b5fc35d5
Richard Clamp <richardc@unixbeard.net>
2013-10-01 21:37:55 +0100

    parse a --debug switch

--- mgollective/command.go
+++ mgollective/command.go
@@ -12,13 +12,32 @@ var client_config_file string
 var server_config_file string
 
 func RunApplication() {
-	flag.Set("logtostderr", "true")
-	flag.Set("stderrthreshold", "0")
-
-	global_flags := flag.NewFlagSet("global", flag.ExitOnError)
-	global_flags.StringVar(&client_config_file, "client-config", "client.cfg", "usage")
-	global_flags.StringVar(&server_config_file, "server-config", "server.cfg", "usage")
+	global_flags := flag.NewFlagSet("mgo", flag.ExitOnError)
+	var debug bool
+	global_flags.BoolVar(
+		&debug,
+		"debug",
+		false,
+		"be very verbose",
+	)
+	global_flags.StringVar(
+		&client_config_file,
+		"client-config",
+		"client.cfg",
+		"specify client config file",
+	)
+	global_flags.StringVar(
+		&server_config_file,
+		"server-config",
+		"server.cfg",
+		"specify server config file",
+	)
 	global_flags.Parse(os.Args[1:])
+
+	if debug {
+		/// tell glog to turn up the noise
+		flag.Set("logtostderr", "true")
+	}
 
 	commander := &commander.Commander{
 		Name:     "mgo",

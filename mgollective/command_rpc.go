19fef8f3bcb92fb72f20bab682a7f9c010bf800d
Richard Clamp <richardc@unixbeard.net>
2013-10-01 21:17:13 +0100

    split args on the cli

--- mgollective/command_rpc.go
+++ mgollective/command_rpc.go
@@ -3,16 +3,28 @@ package mgollective
 import (
 	"code.google.com/p/go-commander"
 	"fmt"
+	"github.com/golang/glog"
+	"strings"
 )
 
 func runRpcCommand(cmd *commander.Command, args []string) {
 	mgo := NewClient()
 
+	if len(args) < 2 {
+		glog.Fatal("not enough arguments")
+	}
+
+	params := make(map[string]string)
+	for _, arg := range args[2:] {
+		value := strings.SplitN(arg, "=", 2)
+		params[value[0]] = value[1]
+	}
+
 	request := RequestMessage{
 		Body: RequestBody{
 			Agent:  args[0],
 			Action: args[1],
-			Params: make(map[string]string),
+			Params: params,
 		},
 	}
 

f80e29f61de5d1ef9faf0651422e13d28b1e661c
Richard Clamp <richardc@unixbeard.net>
2013-10-01 21:27:20 +0100

    simplify flag parsing a little

--- mgollective/command.go
+++ mgollective/command.go
@@ -8,20 +8,17 @@ import (
 )
 
 var commands []*commander.Command
-var client_config_file string = "client.cfg"
-var server_config_file string = "server.cfg"
+var client_config_file string
+var server_config_file string
 
 func RunApplication() {
 	flag.Set("logtostderr", "true")
 	flag.Set("stderrthreshold", "0")
 
 	global_flags := flag.NewFlagSet("global", flag.ExitOnError)
-	cc_file := global_flags.String("client-config", "client.cfg", "usage")
-	sc_file := global_flags.String("server-config", "server.cfg", "usage")
+	global_flags.StringVar(&client_config_file, "client-config", "client.cfg", "usage")
+	global_flags.StringVar(&server_config_file, "server-config", "server.cfg", "usage")
 	global_flags.Parse(os.Args[1:])
-
-	client_config_file = *cc_file
-	server_config_file = *sc_file
 
 	commander := &commander.Commander{
 		Name:     "mgo",

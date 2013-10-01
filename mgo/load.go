2beec0b2cff657ec2df70dce346cf7a81f7358e7
Richard Clamp <richardc@unixbeard.net>
2013-10-01 14:58:38 +0100

    add a null securityprovider

--- mgo/load.go
+++ mgo/load.go
@@ -2,5 +2,6 @@ package main
 
 // for side effects
 import _ "github.com/richardc/mgollective/mgo_connector_activemq"
+import _ "github.com/richardc/mgollective/mgo_securityprovider_null"
 import _ "github.com/richardc/mgollective/mgo_securityprovider_psk"
 import _ "github.com/richardc/mgollective/mgo_encoder_json"
---
+++ mgo_securityprovider_null/mgo_securityprovider_null.go
@@ -0,0 +1,26 @@
+package mgo_securityprovider_null
+
+import (
+	"github.com/richardc/mgollective/mgollective"
+)
+
+type NullSecurityProvider struct {
+}
+
+func (p NullSecurityProvider) Sign(message []byte) map[string]string {
+	headers := make(map[string]string)
+	headers["signature"] = "Everything looks fine buddy"
+	return headers
+}
+
+func (p NullSecurityProvider) Verify(message []byte, headers map[string]string) bool {
+	return true
+}
+
+func makeNullSecurityProvider(app *mgollective.Mgollective) mgollective.SecurityProvider {
+	return &NullSecurityProvider{}
+}
+
+func init() {
+	mgollective.RegisterSecurityProvider("null", makeNullSecurityProvider)
+}

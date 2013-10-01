694b21c6d0d7347ee66ae6cfca1e48843325c112
Richard Clamp <richardc@unixbeard.net>
2013-10-01 15:15:00 +0100

    hash with the psk, rather than just use the psk

--- mgo_securityprovider_psk/securityprovider_psk.go
+++ mgo_securityprovider_psk/securityprovider_psk.go
@@ -1,21 +1,31 @@
 package mgo_securityprovider_psk
 
 import (
+	"crypto/md5"
+	"fmt"
 	"github.com/richardc/mgollective/mgollective"
+	"io"
 )
 
 type PskSecurityProvider struct {
 	psk string
 }
 
+func (s PskSecurityProvider) hash(message []byte) string {
+	hash := md5.New()
+	io.WriteString(hash, s.psk)
+	hash.Write(message)
+	return fmt.Sprintf("%x", hash.Sum(nil))
+}
+
 func (p PskSecurityProvider) Sign(message []byte) map[string]string {
 	headers := make(map[string]string)
-	headers["signature"] = p.psk
+	headers["signature"] = p.hash(message)
 	return headers
 }
 
 func (p PskSecurityProvider) Verify(message []byte, headers map[string]string) bool {
-	if headers["signature"] == p.psk {
+	if headers["signature"] == p.hash(message) {
 		return true
 	}
 	return false

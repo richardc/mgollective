9388d32e387a7a8f9524f320d8155f9200371d53
Richard Clamp <richardc@unixbeard.net>
2013-10-01 22:13:29 +0100

    add -j mode

--- mgollective/command_rpc.go
+++ mgollective/command_rpc.go
@@ -2,6 +2,8 @@ package mgollective
 
 import (
 	"code.google.com/p/go-commander"
+	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/golang/glog"
 	"strings"
@@ -34,15 +36,40 @@ func runRpcCommand(cmd *commander.Command, args []string) {
 
 	discovered_nodes := []string{mgo.Identity()}
 
+	var json_mode bool
+	if cmd.Flag.Lookup("j").Value.String() == "true" {
+		json_mode = true
+	}
+	json_data := make([]interface{}, 0)
+
 	defer mgo.Shutdown()
 	mgo.RpcCommand(request, discovered_nodes, func(message ResponseMessage) {
-		fmt.Printf("%-40s %v\n", message.Headers["mc_identity"], message.Body)
+		if json_mode {
+			record := map[string]interface{}{
+				"agent":  args[0],
+				"action": args[1],
+				"sender": message.Headers["mc_identity"],
+				"body":   message.Body,
+			}
+
+			json_data = append(json_data, record)
+		} else {
+			fmt.Printf("%-40s %v\n", message.Headers["mc_identity"], message.Body)
+		}
 	})
+
+	if json_mode {
+		bytes, _ := json.MarshalIndent(json_data, "  ", "  ")
+		fmt.Print(string(bytes))
+	}
 }
 
 func init() {
-	RegisterCommand(&commander.Command{
+	cmd := &commander.Command{
 		UsageLine: "rpc",
 		Run:       runRpcCommand,
-	})
+		Flag:      *flag.NewFlagSet("mgo-rpc", flag.ExitOnError),
+	}
+	cmd.Flag.Bool("j", false, "output as json")
+	RegisterCommand(cmd)
 }
